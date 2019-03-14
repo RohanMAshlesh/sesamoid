e101ddd30a747fd32ed10640c2b33b534f962439
Rahulmashlesh <rahulmashlesh@gmail.com>
2019-03-14 15:22:47 -0400

    Updated the octave file to step1

--- octave_gui.m
+++ octave_gui.m
@@ -1,11 +1,57 @@
 pkg load image
 
-img = imload("example.png")
+function myplot
+  t_bw = 0.4745099;
+  t_gry = 0.1;
+  t_Noise = 1500;
+  t_pause = 0.51;
+  
+  img_org = imread("images/1.jpg");
+  imshow(img_org);  title('original imgage');  pause(t_pause);
+  
+  %%Check and convert to grey
+  if ndims(img_org) == 3
+    img = rgb2gray(img_org);
+    %print("Image not in grayscale, converting.");
+  else
+    img = img_org;
+  endif
+  
+  imshow(img);  title('grey only img');  pause(t_pause);
+  
+  %%Enhance the brightness
+  img=imadjust(img,[t_gry,1],[]);
+  imshow(img);
+  title('grey enhanced img');
+  pause(t_pause);
+  
+  %%To BW
+  img = im2bw(img, t_bw); 
+  imshow(img);
+  title('BW img');
+  pause(t_pause);
+  
+  %%Noise removal
+  bwareaopen(img,t_Noise);
+  imshow(img);
+  %title('Noise free BW img');
+  pause(t_pause);
+  
+  %% Label connected components
+  [L Ne]=bwlabel(img);
+  %% Measure properties of image regions
+  propied=regionprops(L,'BoundingBox');
+  hold on
+  %% Plot Bounding Box
+  for n=1:size(propied,1)
+    rectangle('Position',propied(n).BoundingBox,'EdgeColor','g','LineWidth',2)
+  end
+  hold off
+  img=img;
+  imshow(img)
+  title('Later ....');
+  
+  
+end
 
-threshold = 0.5
-
-img_bw = im2bw(img, threshold)
-
-imshow(img_grey)
-
-
+myplot()
